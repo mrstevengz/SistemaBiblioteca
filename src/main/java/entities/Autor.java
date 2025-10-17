7dbf68d165fe19b29d4ffaeed536167e7082fabe
gague-uam <ggpaiz@uamv.edu.ni>
2025-10-16 21:10:34 -0600

    Se agrega la entidad Autor

---
+++ src/main/java/entities/Autor.java
@@ -0,0 +1,21 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+@Entity
+@Table(name = "autores")
+@Getter @Setter
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "nombre", length = 100, nullable = false)
+    private String nombre;
+    @Column(name = "nacionalidad", length = 100, nullable = false)
+    private String nacionalidad;
+    @Column(name = "fechanacimiento", nullable = false)
+    private Date fechanacimiento;
+}
