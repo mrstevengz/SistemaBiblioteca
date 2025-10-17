9ceda4d1f9f4311b9f9c6c70faa094eb8173bba7
Fergago20 <fergomez@uamv.edu.ni>
2025-10-16 22:17:41 -0600

    Se agrega funcion guardar

--- src/main/java/dao/daoCategoria.java
+++ src/main/java/dao/daoCategoria.java
@@ -1,4 +1,21 @@
 package dao;
 
-public class daoCategoria {
+import entities.Categoria;
+import jakarta.persistence.EntityManager;
+
+public class daoCategoria implements ICRUD<Categoria>{
+    private final EntityManager em;
+    public CategoriaDAO(EntityManager em) {
+        this.em = em;
+    }
+    @Override
+    public Categoria guardar(Categoria categoria) {
+        if (categoria != null) {
+            em.getTransaction().begin();
+            em.persist(categoria);
+            em.getTransaction().commit();
+            return categoria;
+        }
+        return null;
+    }
 }
